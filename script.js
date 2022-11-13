ef3b6a187cc329d0814919405a69468cbc4c3477
Chopperkasa <limaszandresfelipe@gmail.com>
2022-11-13 16:17:55 -0500

    Se eliminaron funciones innecesarias en el script

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function handleSubmit(event){
     }
 }
 
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+//Función que aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
     var opciones = document.querySelectorAll('#links  a');
     opciones[0].className = "";
@@ -43,20 +43,4 @@ function responsiveMenu() {
     } else {
         x.className = "";
     }
-}
-
-//detecto el scrolling para aplicar la animación del la barra de habilidades
-window.onscroll = function() { efectoHabilidades() };
-
-//funcion que aplica la animación de la barra de habilidades
-function efectoHabilidades() {
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if (distancia_skills >= 300) {
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
-    }
-
 }
